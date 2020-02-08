62167eaf64e570cae4ce836219ee9bd3bb0c5ae0
Jordan T. Bates <jtbates@gmail.com>
2020-02-08 14:25:22 -0700

    Port to Python 3

--- osm2dxf.py
+++ osm2dxf.py
@@ -1,4 +1,4 @@
-from dxfwrite import DXFEngine as dxf
+import ezdxf
 import libxml2
 import sys
 import operator
@@ -7,7 +7,8 @@ from math import pi, tan, log
 def generate_dxf(filename, tags):
     lat2y = lambda lat: (180/pi * log(tan(pi/4+lat*(pi/180)/2)))
     
-    drawing = dxf.drawing(filename + ".dxf")
+    doc = ezdxf.new('R2018')
+    msp = doc.modelspace()
     xml = libxml2.parseFile(filename)
     context = xml.xpathNewContext()
 
@@ -15,7 +16,7 @@ def generate_dxf(filename, tags):
         layer_name = tag.upper()
         paths = context.xpathEval("/*/way[tag/@k = '%s']" %(tag))
         
-        drawing.add_layer(layer_name)
+        doc.layers.new(name=layer_name)
         
         n = context.xpathEval("/*/node")
         lat = {}
@@ -25,10 +26,10 @@ def generate_dxf(filename, tags):
             lat[node.prop('id')] = float(node.prop('lat'))
             lon[node.prop('id')] = float(node.prop('lon'))
         
-        xmax = max(lon.iteritems(), key=operator.itemgetter(1))[1]
-        xmin = min(lon.iteritems(), key=operator.itemgetter(1))[1]
-        ymax = max(lat.iteritems(), key=operator.itemgetter(1))[1]
-        ymin = min(lat.iteritems(), key=operator.itemgetter(1))[1]
+        xmax = max(lon.items(), key=operator.itemgetter(1))[1]
+        xmin = min(lon.items(), key=operator.itemgetter(1))[1]
+        ymax = max(lat.items(), key=operator.itemgetter(1))[1]
+        ymin = min(lat.items(), key=operator.itemgetter(1))[1]
         
         print("Rectangle: [%f, %f], [%f, %f]" % (xmin, xmax, ymin, ymax))
         
@@ -39,8 +40,8 @@ def generate_dxf(filename, tags):
         lat2coord = lambda lat: (lat2y(lat)-basey)*masterscale
         long2coord = lambda lon: (lon-baselong)*masterscale
         
-        print "found %d nodes for %s, cached them" %(len(lat), layer_name)
-        print "found %d paths" %(len(paths))
+        print("found %d nodes for %s, cached them" %(len(lat), layer_name))
+        print("found %d paths" %(len(paths)))
         
         for path in paths:
             # check if path has elevation information
@@ -52,28 +53,27 @@ def generate_dxf(filename, tags):
 
             # find all nodes in path
             nodes = path.xpathEval("nd")
-        
+
             points = []
             closed_path = False
 
             for node in nodes:
-                if lon.has_key(node.prop('ref')) and lat.has_key(node.prop('ref')):
+                if node.prop('ref') in lon and node.prop('ref') in lat:
                     points.append( (long2coord(lon[node.prop('ref')]), lat2coord(lat[node.prop('ref')]), float(elevation)/15.0) )
                 else:
                     print("Key %s not found in lat or long dict! Skipping...")
-
-            polyline = dxf.polyline(points, layer=layer_name)
             
+            polyline = msp.add_lwpolyline(points, dxfattribs={'layer': layer_name})
+
             if nodes[-1].prop('ref') == nodes[0].prop('ref'):
                 closed_path = True
-                polyline.close(status=True)
-        
-            print "Writing %s path for layer %s with elevation=%dm, %d nodes" %("closed" if closed_path else "", layer_name, elevation, len(nodes))
-            drawing.add(polyline)
-        
-    print "Saving file..."
-    drawing.save()
-    print "Done."
+                polyline.close()
+
+            print("Writing %s path for layer %s with elevation=%dm, %d nodes" %("closed" if closed_path else "", layer_name, elevation, len(nodes)))
+
+    print("Saving file...")
+    doc.saveas(filename + ".dxf")
+    print("Done.")
 
 def main(argv):
     filename = argv[1]
